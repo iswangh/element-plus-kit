9183f9d4a46119fa95f0b0dde2c3c1ae74829498
王浩 <wangh.mail@qq.com>
2025-11-29 14:41:28 +0800

    style(form):  格式修改

--- packages/kit/src/types/form.d.ts
+++ packages/kit/src/types/form.d.ts
@@ -1,8 +1,4 @@
 /**
  * Element Plus Kit Form 类型定义导出
  */
-export type {
-  FormActionConfig,
-  FormItemEventExtendedParams,
-  FormItems,
-} from '@iswangh/element-plus-kit-form'
+export type { FormActionConfig, FormItemEventExtendedParams, FormItems } from '@iswangh/element-plus-kit-form'
